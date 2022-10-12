ff3888f1229e9a07ea202954417e23113b3682af
aboalsim114 <sami.abdulhalim@hetic.net>
2022-10-12 23:01:50 +0200

    created footer

---
+++ view/Composants/Nav.php
@@ -0,0 +1,14 @@
+<nav>
+        <div class="logo">
+            <img src="./img//Logo.png" alt="logo" srcset="">
+        </div>
+        <div>
+            <ul class="SMN_effect-77">
+                <li><a href="index.php"><i class="fa-sharp fa-solid fa-house"></i> Acceuil</a></li>
+                <li><a href="connexion.php"><i class="fa-sharp fa-solid fa-right-to-bracket"></i> Connexion</a></li>
+                <li><a href="inscription.php"><i class="fa-solid fa-user-plus"></i> Cree un compte</a></li>
+                <!--                 <button id="btn-deposer-article" type="submit"><i class="fa-sharp fa-solid fa-plus"></i> Déposer une annonce</button>
+ --></ul>
+        </div>
+
+    </nav>
--- view/connexion.php
+++ view/connexion.php
@@ -16,20 +16,7 @@
 
 <body>
 
-    <nav>
-        <div class="logo">
-            <img src="./img//Logo.png" alt="logo" srcset="">
-        </div>
-        <div>
-            <ul class="SMN_effect-77">
-                <li><a href="index.php"><i class="fa-sharp fa-solid fa-house"></i> Acceuil</a></li>
-                <li><a href="connexion.php"><i class="fa-sharp fa-solid fa-right-to-bracket"></i> Connexion</a></li>
-                <li><a href="inscription.php"><i class="fa-solid fa-user-plus"></i> Cree un compte</a></li>
-                <!--                 <button id="btn-deposer-article" type="submit"><i class="fa-sharp fa-solid fa-plus"></i> Déposer une annonce</button>
- --></ul>
-        </div>
-
-    </nav>
+    <?php  require("./Composants/Nav.php")  ?>
 
 
     <section class="vh-100">
@@ -74,7 +61,7 @@
             </div>
         </div>
     </section>
-
+    <?php  require("./Composants/Footer.php")  ?>
     <script src="https://kit.fontawesome.com/36b9253a34.js" crossorigin="anonymous"></script>
 </body>
 
